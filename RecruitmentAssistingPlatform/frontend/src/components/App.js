b0392227476ea8eddba2a5090ec608ada7d35b9d
Arka-h <aurkohaldi@gmail.com>
2020-02-02 03:36:31 +0530

    to be undone

---
+++ RecruitmentAssistingPlatform/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <>
+                <h1>
+                    React App
+                </h1>
+            </>
+        )
+    }
+}
+
+export default App
