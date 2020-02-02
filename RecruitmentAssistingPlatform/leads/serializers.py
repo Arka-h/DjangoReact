4654db4b82f7614c909c54c6260a6b82ed028023
Arka-h <aurkohaldi@gmail.com>
2020-02-02 11:09:04 +0530

    Final BoilerPlate

--- RecruitmentAssistingPlatform/leads/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from leads.models import Leads
-
-#Lead Serializer
-class LeadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Leads
-        fields = '__all__'
